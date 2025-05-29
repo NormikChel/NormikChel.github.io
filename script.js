55b0fda373392651224f927c06ab0b045cba919c
Нормик Чел <nenormikchel@gmail.com>
2025-05-29 12:24:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-// Получаем элементы
-const themeToggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-// Функция для применения сохраненной темы при загрузке страницы
-function applySavedTheme() {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        body.classList.add(savedTheme); // Применяем сохраненную тему
-    } else {
-        body.classList.add('light-theme'); // Устанавливаем светлую тему по умолчанию
-    }
+/* Пример стилей для светлой темы */
+.light-theme {
+    background-color: #f0f0f0;
+    color: #444;
 }
-// Событие для переключения темы
-themeToggleButton.addEventListener('click', () => {
-    // Проверяем текущую тему и переключаем её
-    if (body.classList.contains('dark-theme')) {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-        localStorage.setItem('theme', 'light'); // Сохраняем светлую тему
-    } else {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        localStorage.setItem('theme', 'dark'); // Сохраняем темную тему
-    }
-});
-// Применяем сохраненную тему при загрузке
-applySavedTheme();
+/* Пример стилей для темной темы */
+.dark-theme {
+    background-color: #121212;
+    color: #ffffff;
+}
